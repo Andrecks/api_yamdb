35d24cc7c827e30e748da07c9955c2cbc678ed15
r3t4rdme <72883128+r3t4rdme@users.noreply.github.com>
2021-10-08 01:32:29 +0300

    added models

---
+++ api_yamdb/media/models.py
@@ -0,0 +1,64 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from users.models import User
+
+
+class Categories(models.Model):
+    category_name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Genres(models.Model):
+    genre_name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.genre_name
+
+
+class Titles(models.Model):
+    title_name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    category = models.OneToOneField(
+        Categories, null=True, on_delete=models.SET_NULL, related_name='titles'
+    )
+    genre = models.ForeignKey(
+        Genres, null=True, on_delete=models.SET_NULL, related_name='titles'
+    )
+    year = models.DateTimeField('Год выпуска')
+
+    def __str__(self):
+        return self.title_name
+
+
+class Review(models.Model):
+    score = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)])
+    text = models.TextField()
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews')
+    title = models.OneToOneField(
+        Titles, on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return self.text
